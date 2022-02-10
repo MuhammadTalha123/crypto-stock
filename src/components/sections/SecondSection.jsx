1ee9f53f1b31d2e2e1a53c9e084cab1927b40d73
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2022-02-11 00:15:13 +0500

    Adding right part to second section

--- src/components/sections/SecondSection.jsx
+++ src/components/sections/SecondSection.jsx
@@ -3,6 +3,8 @@ import {
   TabList,
   TabPanels,
   Flex,
+  Text,
+  Button,
   Tab,
   TabPanel,
 } from "@chakra-ui/react";
@@ -64,7 +66,18 @@ export const SecondSection = () => {
               <SectionTwoCard />
               <SectionTwoCard />
             </Flex>
-            <Flex w="48%">Second</Flex>
+            <Flex w="48%" justify="center" align="start" flexDir="column" p="32">
+              <Text fontSize="40px" fontWeight="500">
+                All of your assets, now in one place
+              </Text>
+              <Text>
+                Deposit crypto to the Structure Mobile App and buy stocks,
+                crypto, and other assets in seconds.
+              </Text>
+              <Button bg="black" color="white" mt="10">
+                Browse all assets
+              </Button>
+            </Flex>
           </Flex>
         </TabPanel>
         <TabPanel>
