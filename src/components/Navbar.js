0d9bd2071606c222e7a157fc134ed722d738af25
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2022-02-10 23:01:13 +0500

    Adding navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/layout'
+import { Flex, Text } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
+
+import ICON from "../img/icon.svg";
+import { Button } from '@chakra-ui/button';
 
 const Navbar = () => {
     return (
-        <Flex>
-            navbar
+        <Flex justify="space-around" bg="#F5F6F7" h="68px">
+            <Flex>
+                <Image
+                    width='100px'
+                    src={ICON}
+                    alt='Icon'
+                />
+            </Flex>
+            <Flex justify="center" align="center">
+                <Text mx="15px" fontWeight="500">All Assets</Text>
+                <Text mx="15px" fontWeight="500">Stocks</Text>
+                <Text mx="15px" fontWeight="500">Crypto</Text>
+                <Text mx="15px" fontWeight="500">Trade</Text>
+            </Flex>
+            <Flex justify="center" align="center">
+                <Text mx="15px" fontWeight="500">Sign up</Text>
+                <Button colorScheme="blackAlpha" bg="black">Get the app</Button>
+            </Flex>
         </Flex>
     )
 }
